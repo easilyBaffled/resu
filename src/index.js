bcf91508b1afd7b4ca5ec8549c8688d203aff8cd
Danny Michaelis <dmichaelis0@gmail.com>
2019-03-21 07:52:39 -0400

    update layout

--- src/index.js
+++ src/index.js
@@ -1,26 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Flex, Box } from '@rebass/grid';
+
 
 import "./styles.css";
 import "./rythm.css";
 
 import NPM from "./logos/npm-svg";
 
-const SectionHeader = ({ children }) => <h3 className="section-header">{children}</h3>;
+const SectionHeader = ({ children }) => <h2 className="section-header important"><span className="underline">{children}</span></h2>;
 const ItemHeader = ({ children }) => <h4 className="item-header">{children}</h4>;
+const IH = ({ children }) => <Box as='h4' className="item-header" width={1/2}>{children}</Box>;
+const ProfessionalExperience = ( { name, dateRange, title, points } ) =>
+    <div>
+        <Flex><IH>{title}</IH><IH>{dateRange}</IH></Flex>
+        <div><h4>{name}</h4></div>
+        <ul>
+            {
+                points.map( point => <li key={point}>{point}</li> )
+            }
+        </ul>
+    </div>;
 
 const PersonalProject = ({ name, icon: Icon, content }) => (
   <div>
-    <ItemHeader>
+    <IH>
         <span>{name}</span>
       <Icon/>
-    </ItemHeader>
+    </IH>
     {content()}
   </div>
 );
 
 const sections = {
-  personalProjects: [
+    professionalExperience: [
+        {
+            title: 'Software Engineer',
+            dateRange: 'December \'17 - Current',
+            name: 'Lab49',
+            points: [
+            'Built UI for financial web platform with modern Javascript',
+            'Built experimental dashboard for service health reporting',
+            'Iterated on existing company best practices',
+            'Developed and demoed new possible practices and tools such as Git Hooks, GraphQL, and Property-Based Testing'
+            ]
+
+
+
+        }
+    ],
+    personalProjects: [
       {
           name: "N/A",
           icon: () => "-",
@@ -77,17 +106,23 @@ const sections = {
 function App() {
   return (
     <div className="page">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
         <header>
             <span>
-                <h1>
+                <h1 className="important">
                     Danny Michaelis
                 </h1>
             </span>
             <span></span>
         </header>
-        <section></section>
+        <section className="body">
+            <SectionHeader>Professional Experience</SectionHeader>
+            {
+                sections.professionalExperience.map(
+                    experiance => <ProfessionalExperience key={experiance.name} {...experiance}/>
+                )
+
+            }
+        </section>
         <footer>
         <SectionHeader>Personal Projects</SectionHeader>
         <div className="grid">
