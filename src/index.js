c5b5cff0e133203db130adfea35fb6c9dacbb61e
Danny Michaelis <dmichaelis0@gmail.com>
2019-03-24 11:30:08 -0400

    - apply prettier - bump spacing around tools bar

--- src/index.js
+++ src/index.js
@@ -5,41 +5,47 @@ import styled from "@emotion/styled";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Flex, Box } from "@rebass/grid/emotion";
-import { GraphQL, JQuery, ReactLogo, Redux, Sass, Nodejs, Angular } from 'emotion-icons/simpleIcons';
+import {
+  GraphQL,
+  JQuery,
+  ReactLogo,
+  Redux,
+  Sass,
+  Nodejs,
+  Angular
+} from "emotion-icons/simpleIcons";
 
 import "./styles.css";
 import "./rythm.css";
 
 import sections from "./sections";
 
-console.ident = ( v, l = '' ) => ( console.log( l, v ), v );
+console.ident = (v, l = "") => (console.log(l, v), v);
 
 const flags = {
-    skillBar: true
+  skillBar: true
 };
 
 /*
-* ==================
-* Styles
-* ==================
-* */
+ * ==================
+ * Styles
+ * ==================
+ * */
 const sectionBorders = {
-    top: css({ borderTop: '2px solid black'}),
-    right: css({ borderRight: '2px solid black'}),
-    bottom: css({ borderBottom: '2px solid black'}),
-    left: css({ borderLeft: '2px solid black'}),
-}
+  top: css({ borderTop: "2px solid black" }),
+  right: css({ borderRight: "2px solid black" }),
+  bottom: css({ borderBottom: "2px solid black" }),
+  left: css({ borderLeft: "2px solid black" })
+};
 
 const headerDataBorders = {
-    top: css({ borderTop: '1px solid lightgrey'}),
-    right: css({ borderRight: '1px solid lightgrey'}),
-    bottom: css({ borderBottom: '1px solid lightgrey'}),
-    left: css({ borderLeft: '1px solid lightgrey'}),
-}
+  top: css({ borderTop: "1px solid lightgrey" }),
+  right: css({ borderRight: "1px solid lightgrey" }),
+  bottom: css({ borderBottom: "1px solid lightgrey" }),
+  left: css({ borderLeft: "1px solid lightgrey" })
+};
 
-
-
-const negateMargin = css({ margin: 0 } );
+const negateMargin = css({ margin: 0 });
 
 const centered = css({
   display: "flex",
@@ -52,9 +58,9 @@ const important = css({
 
 const Underlined = styled.span({
   paddingRight: "1.5rem",
-    paddingTop: '.5rem',
-    paddingBottom: '.5rem',
-    borderBottom: "2px solid #090909"
+  paddingTop: ".5rem",
+  paddingBottom: ".5rem",
+  borderBottom: "2px solid #090909"
 });
 
 const ImportantHeader = styled.h3(
@@ -65,34 +71,45 @@ const ImportantHeader = styled.h3(
   important
 );
 
-const ToolsBar = ( { size = 1 } ) =>
-    flags.skillBar &&
-    <Box width={1}>
-        <Flex alignItems="center" css={css({ '& *': { marginRight: 20, marginBottom: '.5rem', height: `${Number(size)+.5}rem`, fontSize: `${size}rem` } })}>
-            <h4>Tools:</h4>
-            <Flex alignItems="center">
-                <GraphQL /> <p>GraphQL</p>
-            </Flex>
-            <Flex alignItems="center">
-                <ReactLogo /> <p>React</p>
-            </Flex>
-            <Flex alignItems="center">
-                <Redux  /> <p>Redux</p>
-            </Flex>
-            <Flex alignItems="center">
-                <JQuery  /> <p>JQuery</p>
-            </Flex>
-            <Flex alignItems="center">
-                <Sass  /> <p>Sass</p>
-            </Flex>
-            <Flex alignItems="center">
-                <Nodejs  /> <p>Node.js</p>
-            </Flex>
-            <Flex alignItems="center">
-                <Angular  /> <p>Angular</p>
-            </Flex>
+const ToolsBar = ({ size = 1 }) =>
+  flags.skillBar && (
+    <Box width={1} css={[headerDataBorders.top, css({padding: ".5rem 0"})]} >
+      <Flex
+        alignItems="center"
+        css={css({
+          "& *": {
+            marginRight: 20,
+            marginBottom: 0,
+            height: `${Number(size) + 0.5}rem`,
+            fontSize: `${size}rem`
+          }
+        })}
+      >
+        <h4>Tools:</h4>
+        <Flex alignItems="center">
+          <GraphQL /> <p>GraphQL</p>
         </Flex>
-    </Box>;
+        <Flex alignItems="center">
+          <ReactLogo /> <p>React</p>
+        </Flex>
+        <Flex alignItems="center">
+          <Redux /> <p>Redux</p>
+        </Flex>
+        <Flex alignItems="center">
+          <JQuery /> <p>JQuery</p>
+        </Flex>
+        <Flex alignItems="center">
+          <Sass /> <p>Sass</p>
+        </Flex>
+        <Flex alignItems="center">
+          <Nodejs /> <p>Node.js</p>
+        </Flex>
+        <Flex alignItems="center">
+          <Angular /> <p>Angular</p>
+        </Flex>
+      </Flex>
+    </Box>
+  );
 
 const SectionHeader = ({ children }) => (
   <ImportantHeader>
@@ -110,22 +127,70 @@ const ItemHeader = styled.h5(
   centered
 );
 
-// match css rule (\w+)(?:-(\w+))?(?:-(\w+))?(?:-(\w+))?:\s*([^;]+)
+const Header = () =>
+    <Flex as="header" css={[centered, sectionBorders.bottom]}>
+        <Box width={1 / 4} as="h1" css={[important]}>
+            Danny Michaelis
+        </Box>
+        <Box width={3 / 4} css={sectionBorders.left}>
+            <Flex flexWrap="wrap">
+                <Box
+                    width={1}
+                    as="h4"
+                    css={[important, splitPadding, headerDataBorders.bottom]}
+                >
+                    2122 Massachusetts Ave NW Washington, DC 20008
+                </Box>
+                <Box
+                    width={1 / 2}
+                    as="h4"
+                    css={[
+                        important,
+                        splitPadding,
+                        headerDataBorders.bottom,
+                        headerDataBorders.right
+                    ]}
+                >
+                    Email: dmichaelis0@gmail.com
+                </Box>
+                <Box
+                    width={1 / 2}
+                    as="h4"
+                    css={[important, splitPadding, headerDataBorders.bottom]}
+                >
+                    Cell Phone: 973-518-0044
+                </Box>
+
+                <Box
+                    width={1 / 2}
+                    as="h4"
+                    css={[important, splitPadding, headerDataBorders.right]}
+                >
+                    Github: <a href="https://goo.gl/rylpYp">https://goo.gl/rylpYp</a>
+                </Box>
+                <Box width={1 / 2} as="h4" css={[important, splitPadding]}>
+                    LinkedIn:{" "}
+                    <a href="https://goo.gl/FMhSPF">https://goo.gl/FMhSPF</a>
+                </Box>
+            </Flex>
+        </Box>
+    </Flex>;
+
 
 const ProfessionalExperience = ({ name, dateRange, title, points }) => (
-    <Box width={1} >
-        <Flex justifyContent="space-between">
-            <ItemHeader>{title}</ItemHeader>
-            <ItemHeader>{dateRange}</ItemHeader>
-        </Flex>
-        <ItemHeader>{name}</ItemHeader>
-        <ul style={{ padding: "0 2rem"}}>
-          {points.map(point => (
-            <li key={point}>{point}</li>
-          ))}
-        </ul>
-        <ToolsBar size={.8}/>
-        <Box width={2/3} mb={3} css={ name !== 'AOL' && sectionBorders.bottom } />
+  <Box width={1}>
+    <Flex justifyContent="space-between">
+      <ItemHeader>{title}</ItemHeader>
+      <ItemHeader>{dateRange}</ItemHeader>
+    </Flex>
+    <ItemHeader>{name}</ItemHeader>
+    <ul style={{ padding: "0 2rem 1rem" }}>
+      {points.map(point => (
+        <li key={point}>{point}</li>
+      ))}
+    </ul>
+    <ToolsBar size={0.8} />
+    <Box width={2 / 3} mb={3} css={name !== "AOL" && sectionBorders.bottom} />
   </Box>
 );
 
@@ -140,58 +205,32 @@ const PersonalProject = ({ name, icon: Icon, content }) => (
 );
 
 const splitPadding = css({
-    padding: '.5rem .5rem .5rem'
+  padding: ".5rem .5rem .5rem"
 });
 
 function App() {
   return (
     <div className="page">
-      <Flex as="header" css={[centered, sectionBorders.bottom]}>
-        <Box width={1 / 4} as="h1" css={[important]}>
-          Danny Michaelis
-        </Box>
-        <Box width={3 / 4} css={sectionBorders.left}>
-          <Flex flexWrap="wrap">
-            <Box width={1} as="h4" css={[important, splitPadding,  headerDataBorders.bottom]}>
-              2122 Massachusetts Ave NW Washington, DC 20008
-            </Box>
-            <Box width={1 / 2} as="h4" css={[important, splitPadding, headerDataBorders.bottom, headerDataBorders.right]}>
-              Email: dmichaelis0@gmail.com
-            </Box>
-            <Box width={1 / 2} as="h4" css={[important, splitPadding, headerDataBorders.bottom]}>
-              Cell Phone: 973-518-0044
-            </Box>
-
-            <Box width={1 / 2} as="h4" css={[important, splitPadding, headerDataBorders.right]}>
-              Github: <a href="https://goo.gl/rylpYp">https://goo.gl/rylpYp</a>
-            </Box>
-            <Box width={1 / 2} as="h4" css={[important, splitPadding]}>
-              LinkedIn:{" "}
-              <a href="https://goo.gl/FMhSPF">https://goo.gl/FMhSPF</a>
-            </Box>
-          </Flex>
-        </Box>
-      </Flex>
-
+    <Header />
       <section className="body">
         <SectionHeader>Professional Experience</SectionHeader>
-        {sections.professionalExperience.map(experiance => (
-          <ProfessionalExperience key={experiance.name} {...experiance} />
+        {sections.professionalExperience.map( ( experiance, i ) => (
+          <ProfessionalExperience key={i} {...experiance} />
         ))}
       </section>
       <footer css={sectionBorders.top}>
         <SectionHeader>Personal Projects</SectionHeader>
         <div className="grid">
-          {sections.personalProjects.map(project => (
-            <PersonalProject key={project.name} {...project} />
+          {sections.personalProjects.map( ( project, i ) => (
+            <PersonalProject key={i} {...project} />
           ))}
         </div>
       </footer>
-        <h4>
-            <span css={important}>Education </span>
-            University of Maryland College Park Bachelors of Science Computer
-            Science - May 2014
-        </h4>
+      <h4>
+        <span css={important}>Education </span>
+        University of Maryland College Park Bachelors of Science Computer
+        Science - May 2014
+      </h4>
     </div>
   );
 }
