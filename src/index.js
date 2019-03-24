b857636923c5d0e739edab0ddc58e65087eaefe4
Danny Michaelis <dmichaelis0@gmail.com>
2019-03-24 13:12:34 -0400

    - add gotham font - increase font size - add header icons to flags

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ import {
   Angular
 } from "emotion-icons/simpleIcons";
 
-import { School, Phone, Email, LocationCity } from 'emotion-icons/material';
-import { MarkGithub } from 'emotion-icons/octicons';
+import { School, Phone, Email, LocationCity } from "emotion-icons/material";
+import { MarkGithub } from "emotion-icons/octicons";
 
 import "./styles.css";
 import "./rythm.css";
@@ -26,7 +26,8 @@ import sections from "./sections";
 console.ident = (v, l = "") => (console.log(l, v), v);
 
 const flags = {
-  skillBar: true
+  skillBar: true,
+  useIcon: true
 };
 
 /*
@@ -47,8 +48,6 @@ const headerDataBorders = {
   bottom: css({ borderBottom: "1px solid lightgrey" }),
   left: css({ borderLeft: "1px solid lightgrey" })
 };
-
-const negateMargin = css({ margin: 0 });
 
 const centered = css({
   display: "flex",
@@ -76,7 +75,7 @@ const ImportantHeader = styled.h3(
 
 const ToolsBar = ({ size = 1 }) =>
   flags.skillBar && (
-    <Box width={1} css={[headerDataBorders.top, css({padding: ".5rem 0"})]} >
+    <Box width={1} css={[headerDataBorders.top, css({ padding: ".5rem 0" })]}>
       <Flex
         alignItems="center"
         css={css({
@@ -84,7 +83,7 @@ const ToolsBar = ({ size = 1 }) =>
             marginRight: 20,
             marginBottom: 0,
             height: `${Number(size) + 0.5}rem`,
-            fontSize: `${size}rem`,
+            fontSize: `${size}rem`
           }
         })}
       >
@@ -130,56 +129,56 @@ const ItemHeader = styled.h5(
   centered
 );
 
-const Header = () =>
-    <Flex as="header" css={[centered, sectionBorders.bottom]}>
-        <Box width={1 / 4} as="h1" css={[blueText, css({ padding: 0 })]}>
-            Danny Michaelis
+const Header = () => (
+  <Flex as="header" css={[centered, sectionBorders.bottom]}>
+    <Box width={1 / 4} as="h1" css={[blueText, css({ padding: 0 })]}>
+      Danny Michaelis
+    </Box>
+    <Box width={3 / 4} css={sectionBorders.left}>
+      <Flex flexWrap="wrap" css={css({ "& *": { color: "#73AFD9" } })}>
+        {" "}
+        {/*Experimental, making the colors lighter so it pulls less attention*/}
+        <Box
+          width={1 / 2}
+          as="h5"
+          css={[splitPadding, headerDataBorders.bottom]}
+        >
+          {flags.useIcon ? <LocationCity /> : ""}Washington, DC 20009
         </Box>
-        <Box width={3 / 4} css={sectionBorders.left}>
-            <Flex flexWrap="wrap" css={css({ '& *': { color: '#73AFD9' }  })}> {/*Experimental, making the colors lighter so it pulls less attention*/}
-                <Box
-                    width={1/2}
-                    as="h5"
-                    css={[splitPadding, headerDataBorders.bottom]}
-                >
-                    { useIcon ? <LocationCity/>  : '' }Washington, DC 20009
-                </Box>
-                <Box
-                    width={1 / 2}
-                    as="h5"
-                    css={[
-                        splitPadding,
-                        headerDataBorders.bottom,
-                        headerDataBorders.left
-                    ]}
-                >
-                    { useIcon ? <Email/>  : 'Email:' } dmichaelis0@gmail.com
-                </Box>
-                <Box
-                    width={1 / 2}
-                    as="h5"
-                    css={[splitPadding, headerDataBorders.bottom]}
-                >
-                    { useIcon ? <Phone/>  : 'Phone:' } 973-518-0044
-                </Box>
-                <Box
-                    width={1 / 2}
-                    as="h5"
-                    css={[splitPadding, headerDataBorders.left, headerDataBorders.bottom]}
-                >
-                    { useIcon ? <MarkGithub/>  : '' } Github: <a href="https://goo.gl/rylpYp">https://goo.gl/rylpYp</a>
-                </Box>
-                <Box as="h5">
-                    { useIcon ? <School/>  : 'School:' } University of Maryland College Park BS Computer Science - May 2014
-                </Box>
-                {/*<Box width={1 / 2} as="h5" css={[important, splitPadding]}>*/}
-                    {/*LinkedIn:{" "}*/}
-                    {/*<a href="https://goo.gl/FMhSPF">https://goo.gl/FMhSPF</a>*/}
-                {/*</Box>*/}
-            </Flex>
+        <Box
+          width={1 / 2}
+          as="h5"
+          css={[splitPadding, headerDataBorders.bottom, headerDataBorders.left]}
+        >
+          {flags.useIcon ? <Email /> : "Email:"} dmichaelis0@gmail.com
         </Box>
-    </Flex>;
-
+        <Box
+          width={1 / 2}
+          as="h5"
+          css={[splitPadding, headerDataBorders.bottom]}
+        >
+          {flags.useIcon ? <Phone /> : "Phone:"} 973-518-0044
+        </Box>
+        <Box
+          width={1 / 2}
+          as="h5"
+          css={[splitPadding, headerDataBorders.left, headerDataBorders.bottom]}
+        >
+          {flags.useIcon ? <MarkGithub /> : ""} Github:{" "}
+          <a href="https://goo.gl/rylpYp">https://goo.gl/rylpYp</a>
+        </Box>
+        <Box as="h5">
+          {flags.useIcon ? <School /> : "School:"} University of Maryland
+          College Park BS Computer Science - May 2014
+        </Box>
+        {/*<Box width={1 / 2} as="h5" css={[important, splitPadding]}>*/}
+        {/*LinkedIn:{" "}*/}
+        {/*<a href="https://goo.gl/FMhSPF">https://goo.gl/FMhSPF</a>*/}
+        {/*</Box>*/}
+      </Flex>
+    </Box>
+  </Flex>
+);
 
 const ProfessionalExperience = ({ name, dateRange, title, points }) => (
   <Box width={1}>
@@ -211,22 +210,21 @@ const PersonalProject = ({ name, icon: Icon, content }) => (
 const splitPadding = css({
   padding: ".5rem .5rem .5rem"
 });
-const useIcon = true;
 
 function App() {
   return (
     <div className="page">
-     <Header />
+      <Header />
       <section className="body">
         <SectionHeader>Professional Experience</SectionHeader>
-        {sections.professionalExperience.map( ( experiance, i ) => (
+        {sections.professionalExperience.map((experiance, i) => (
           <ProfessionalExperience key={i} {...experiance} />
         ))}
       </section>
       <footer css={sectionBorders.top}>
         <SectionHeader>Personal Projects</SectionHeader>
         <div className="grid">
-          {sections.personalProjects.map( ( project, i ) => (
+          {sections.personalProjects.map((project, i) => (
             <PersonalProject key={i} {...project} />
           ))}
         </div>
